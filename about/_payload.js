a19d45f45cec155dde58cd75e42b8f7d2de830b4
Bahman75 <78180207+Bahman75@users.noreply.github.com>
2023-07-10 15:41:10 +0200

    Updates

--- about/_payload.js
+++ about/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1688996238840}
+export default {data:{},prerenderedAt:1688996466722}
--- contact/_payload.js
+++ contact/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1688996238842}
+export default {data:{},prerenderedAt:1688996466724}
